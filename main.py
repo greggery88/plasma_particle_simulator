586576f4b8f3f75c07c6c3fac20574f70643179f
jackwilliamson <medbwi@gmail.com>
2024-06-18 15:19:20 -0400

    update

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import mpl_toolkits.mplot3d.axes3d as p3
 
 particle_dic = {
-    "electron": {"charge": -1.602 * 10**-19, "mass": 9.11 * 10 * -31},
-    "proton": {"charge": 1.602 * 10**-19, "mass": 1.67 * 10**-27},
+    "electron": {"charge": -1.602 * 10**-19, "mass": 9.11 * 10**-31, "color": "b"},
+    "proton": {"charge": 1.602 * 10**-19, "mass": 1.67 * 10**-27, "color": "r"},
 }
 
-bo = np.array([0, 0, 1])  # the magnitude of the magnetic field.
-E = np.array([0, 0, -1])
-t = 0
+bo = np.array([0, 0, 10**-9])  # the magnitude of the magnetic field.
+E = np.array([0, 0, 0])
+t = np.linspace(0, 100, 100)
+x_values = []
+y_values = []
+z_values = []
 
 
 class Particle:
@@ -17,32 +22,52 @@ class Particle:
         self.y = p_y
         self.z = p_z
         self.pos = np.array([self.x, self.y, self.z])
-        self.vx = 1
-        self.vy = 0
-        self.vz = 0
-        self.v = np.array([self.vx, self.vy, self.vz])
+        self.v = np.array([1, 0, 0])
         self.q = p_type["charge"]
         self.m = p_type["mass"]
+        self.color = p_type["color"]
 
     def force_calc(self):
-        a = self.q * (E + np.cross(self.v, bo)) / self.m
-        self.v = self.v + a
+        F = self.q * (E + np.cross(self.v, bo))
+        a = F / self.m
+        self.v = a
 
     def particle_motion(self):
         self.pos = self.v + self.pos
+        print("hi")
+
+
+def animate_values(t_max, p):
+    x_values.append(p.x)
+    y_values.append(p.y)
+    z_values.append(p.z)
+    for _ in t_max:
+        p.force_calc()
+        p.particle_motion()
+        print(p.x)
+        x_values.append(p.x)
+        y_values.append(p.y)
+        z_values.append(p.z)
 
 
 # run function
 def main():
-    ps = [Particle(1, 0, 0, particle_dic["electron"]) for _ in range(1)]
-    
-    # set up viewer
-    plt.show()
+    # set up plots
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
+    ax = fig.add_subplot(projection="3d")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z")
+    # set up particles
+    p = Particle(1, 0, 0, particle_dic["electron"])
+    animate_values(t, p)
+    print(x_values)
+    print(y_values)
+    print(z_values)
+    scat = ax.scatter(p.x, p.y, p.z, marker="v", c=p.color)
+
+    # set up viewer
+    plt.show()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,10 +1,47 @@
+import unittest
 import numpy as np
+import main as ma
 
 
-a_x = np.array([1, 0, 0])
-a_y = np.array([0, 1, 0])
-a_z = np.array([0, 0, 0])
+class TestMyMath(unittest.TestCase):
+    def test_lorentz_law(self):
+        p = ma.Particle(0, 0, 0, ma.particle_dic["electron"])
 
-a_n = np.cross(a_x, a_y)
+        # starting const
+        ma.E = np.array([0, 0, 0])
+        ma.bo = np.array([0, 0, 0])
+        p.v = np.array([1, 0, 0])
 
-print(a_n)
+        # operation
+        p.force_calc()
+
+        # assert =
+        u = p.v[0]
+        v = p.v[1]
+        w = p.v[2]
+
+        self.assertEqual(u, 1)
+        self.assertEqual(v, 0)
+        self.assertEqual(w, 0)
+
+    def test_lorentz_law_2(self):
+        p = ma.Particle(0, 0, 0, ma.particle_dic["electron"])
+        ma.E = np.array([0, 0, 0])
+        ma.bo = np.array([0, 0, 1])
+        p.v = np.array([1, 0, 0])
+
+        # operation
+        p.force_calc()
+
+        # assert =
+        u = p.v[0]
+        v = p.v[1]
+        w = p.v[2]
+
+        self.assertEqual(u, 1)
+        self.assertEqual(v, 1.758 * 10**11)
+        self.assertEqual(w, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
