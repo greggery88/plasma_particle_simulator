951d06010ea7a91734a830f244c4a3ea47725d8d
greggery88 <medbwi@gmail.com>
2024-06-23 16:21:11 -0400

    update

---
+++ lorentzforce_p_sim.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from itertools import count
+import logging
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+
+    # universal constance.
+    e = -1.602 * 10**-19
+    k = 8.99 * 10**9
+    c = 3 * 10**8
+
+    # counter
+    n = count()
+    # fields
+    ef = np.array([0, 0, 1])
+    bf = np.array([0, 0, 1])
+
+    # particle dictionary
+    pd = {
+        "proton": {"m": 1.67 * 10**-27, "Q": -e, "c": "r", "view": 1},
+        "electron": {"m": 9.11 * 10**-31, "Q": e, "c": "b", "view": 1},
+    }
+
+    # calc mag
+    def calc_mag(vector):
+        n = 0
+        for i in range(len(vector)):
+
+            n += vector[i] ** 2
+        return np.sqrt(n)
+
+    class Particle:
+
+        def __init__(self, p_type):
+            # particle scaler s
+            self.q = p_type["Q"]
+            self.m = p_type["m"]
+            self.c = p_type["c"]
+            self.alpha = np.random.uniform(0.6, 0.9)
+
+            # particle previous positions
+            self.xs = []
+            self.ys = []
+            self.zs = []
+
+            # particle vectors
+            self.pos = np.random.uniform(-1, 1, size=3)
+            self.velo = np.array([1, 0, 0])
+            self.accel = np.array([0, 0, 0])
+
+        def calc_pos(self, t, ps, es):
+            debye_length = 10
+            f_tot = np.zeros(3)
+            if self.q != 0:
+                print(np.cross(self.velo, bf))
+                f = 10 * 3 * np.cross(self.velo, bf) / calc_mag(np.cross(self.velo, bf))
+
+                f_tot = f_tot + f
+            # internal electric fields
+            for p in ps:
+                if p != self:
+                    displacement = p.pos - self.pos
+                    distance = np.sqrt(
+                        displacement[0] ** 2
+                        + displacement[1] ** 2
+                        + displacement[2] ** 2
+                    )
+                    if distance < debye_length:
+                        f = -(
+                            1
+                            / 2
+                            * ((k * self.q * p.q) / (distance**2))
+                            * displacement
+                            / distance
+                        )
+                        f_tot = f + f_tot
+            for p in es:
+                if p != self:
+                    displacement = p.pos - self.pos
+                    distance = np.sqrt(
+                        displacement[0] ** 2
+                        + displacement[1] ** 2
+                        + displacement[2] ** 2
+                    )
+                    if distance < debye_length:
+                        f = -(
+                            1
+                            / 2
+                            * ((k * self.q * p.q) / (distance**2))
+                            * displacement
+                            / distance
+                        )
+                        f_tot = f + f_tot
+
+            # find the posiont from the forces
+            self.accel = f_tot
+            self.velo = self.velo + self.accel
+            self.pos = self.pos + self.velo
+
+            # append position
+            self.xs.append(self.pos[0])
+            self.ys.append(self.pos[1])
+            self.zs.append(self.pos[2])
+
+        def draw(self):
+            plt.plot(self.xs, self.ys, self.zs, c=self.c, alpha=(self.alpha - 0.59))
+            plt.plot(
+                self.pos[0],
+                self.pos[1],
+                self.pos[2],
+                c=self.c,
+                alpha=self.alpha,
+                marker=".",
+            )
+
+    particles_p = [Particle(pd["proton"]) for _ in range(1)]
+    particles_e = [Particle(pd["electron"]) for _ in range(0)]
+
+    def animate_particle(t):
+        log.info(next(n))
+        for particle in particles_p:
+            particle.calc_pos(t, particles_p, particles_e)
+        for particle in particles_e:
+            particle.calc_pos(t, particles_p, particles_e)
+
+        plt.cla()
+        for particle in particles_p:
+            particle.draw()
+        for particle in particles_e:
+            particle.draw()
+
+    def on_close_event(event):  # close function
+        print(event)
+        quit()
+
+    # animation function
+    # noinspection PyUnusedLocal,PyTypeChecker
+    anim = FuncAnimation(plt.gcf(), animate_particle, frames=100, repeat=False)
+    fig.canvas.mpl_connect("close_event", on_close_event)
+
+    # print graphic
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,4 @@
+import particlefieldint as pfi
+
+if __name__ == "__main__":
+    pfi.main(particle_type="proton", case="2")
