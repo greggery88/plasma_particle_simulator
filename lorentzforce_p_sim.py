77ff6048256437c0818b228e8ecbf86a8dd59327
jwilliamson <medbwi@gmail.com>
2024-06-24 00:58:40 -0400

    update

--- lorentzforce_p_sim.py
+++ lorentzforce_p_sim.py
@@ -7,42 +7,51 @@ import logging
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+# universal constance.
+e = -1.602 * 10**-19
+k = 8.99 * 10**9
+
+# particle dictionary
+pd = {
+    "proton": {"m": 1.67 * 10**-27, "Q": -e, "c": "r", "view": 1},
+    "electron": {"m": 9.11 * 10**-31, "Q": e, "c": "b", "view": 1},
+}
+
+
+def magnetic_field():
+    magnetic_field_ = np.array([0, 0, 1])
+    return magnetic_field_
+
+
+def electric_field():
+    electric_field_ = np.array([0, 0, 1])
+    return electric_field_
+
+
+def calculate_vector_mag(vector):
+    n = 0
+    for i in range(len(vector)):
+
+        n += vector[i] ** 2
+    return np.sqrt(n)
+
 
 def main():
     fig = plt.figure()
+    # noinspection PyUnusedLocal
     ax = fig.add_subplot(projection="3d")
-
-    # universal constance.
-    e = -1.602 * 10**-19
-    k = 8.99 * 10**9
-    c = 3 * 10**8
 
     # counter
     n = count()
-    # fields
-    ef = np.array([0, 0, 1])
-    bf = np.array([0, 0, 1])
+    nn = count()
 
-    # particle dictionary
-    pd = {
-        "proton": {"m": 1.67 * 10**-27, "Q": -e, "c": "r", "view": 1},
-        "electron": {"m": 9.11 * 10**-31, "Q": e, "c": "b", "view": 1},
-    }
-
-    # calc mag
-    def calc_mag(vector):
-        n = 0
-        for i in range(len(vector)):
-
-            n += vector[i] ** 2
-        return np.sqrt(n)
-
+    # particle
     class Particle:
 
         def __init__(self, p_type):
             # particle scaler s
-            self.q = p_type["Q"]
-            self.m = p_type["m"]
+            self.charge = p_type["Q"]
+            self.mass = p_type["m"]
             self.c = p_type["c"]
             self.alpha = np.random.uniform(0.6, 0.9)
 
@@ -52,94 +61,97 @@ def main():
             self.zs = []
 
             # particle vectors
-            self.pos = np.random.uniform(-1, 1, size=3)
-            self.velo = np.array([0, 0, 0])
-            self.accel = np.array([0, 0, 0])
+            self.position = np.random.uniform(-0.0001, 0.0001, size=3)
+            self.velocity = np.array([1, 0, 0])
+            self.a = np.array([0, 0, 0])
 
-        def calc_pos(self, t, ps, es):
-            debye_length = 1
-            f_tot = np.zeros(3)
-            timestep = 0.00000000001
-            # print(np.cross(self.velo, bf))
-            f = self.q * (ef + np.cross(self.velo, bf))  # -   self.q * (self.velo / c**2) * (np.dot(self.velo, ef))
+        # noinspection PyUnresolvedReferences
+        def calculate_pos_v_a(self, particles_):
+            # set debye length
+            debye_length = 5
 
-            # f = f / lorentz_facter
+            # find the force
+            # forces
 
-            f_tot = f_tot + f
-            # internal electric fields
-            for p in ps:
+            resultant_force = np.zeros(3)
+            # particle particle interactions
+
+            resultant_force += self.particle_interactions(particles_, debye_length)
+
+            # particle external field interactions
+            resultant_force += self.external_field()
+
+            # timestep to add detail for the integrate
+            # edit to slow down or speed up.
+            # speeding up the graph removes detail.
+            timestep = 0.000000000001
+
+            # calculates acceleration
+            self.a = resultant_force / self.mass
+
+            # calculates velocity
+            self.velocity += self.a * timestep
+
+            # calculates position
+            self.position = self.position + self.velocity * timestep
+
+            self.xs.append(self.position[0])
+            self.ys.append(self.position[1])
+            self.zs.append(self.position[2])
+
+        def particle_interactions(self, particles_, debye_length):
+            resultant_force = np.zeros(3)
+            for p in particles_:
                 if p != self:
-                    displacement = p.pos - self.pos
+                    displacement = p.position - self.position
                     distance = np.sqrt(
                         displacement[0] ** 2
                         + displacement[1] ** 2
                         + displacement[2] ** 2
                     )
                     if distance < debye_length:
-                        f = -(
+                        force = -(
                             1
                             / 2
-                            * ((k * self.q * p.q) / (distance**2))
+                            * ((k * self.charge * p.charge) / (distance**2))
                             * displacement
                             / distance
                         )
-                        f_tot = f + f_tot
-            for p in es:
-                if p != self:
-                    displacement = p.pos - self.pos
-                    distance = np.sqrt(
-                        displacement[0] ** 2
-                        + displacement[1] ** 2
-                        + displacement[2] ** 2
-                    )
-                    if distance < debye_length:
-                        f = -(
-                            1
-                            / 2
-                            * ((k * self.q * p.q) / (distance**2))
-                            * displacement
-                            / distance
-                        )
-                        f_tot += f
+                        resultant_force += force
+                        print(resultant_force)
+            return resultant_force
 
-            # find the posiont from the forces
-            self.accel = f_tot / self.m
-            self.velo = self.velo + self.accel * timestep
-            if calc_mag(self.velo) > 3.0 * 10**8:
-                self.velo = 3.0*10**8 * self.velo / calc_mag(self.velo)
-            self.pos += self.velo * timestep
-
-            # append position
-            self.xs.append(self.pos[0])
-            self.ys.append(self.pos[1])
-            self.zs.append(self.pos[2])
+        def external_field(self):
+            # noinspection PyUnreachableCode
+            field_force = self.charge * (
+                electric_field() + np.cross(self.velocity, magnetic_field())
+            )
+            return field_force
 
         def draw(self):
-            plt.plot(self.xs, self.ys, self.zs, c=self.c, alpha=(self.alpha - 0.1))
+
+            # plot the points
+            plt.plot(self.xs, self.ys, self.zs, c=self.c, alpha=(self.alpha - 0.4))
             plt.plot(
-                self.pos[0],
-                self.pos[1],
-                self.pos[2],
+                self.position[0],
+                self.position[1],
+                self.position[2],
                 c=self.c,
                 alpha=self.alpha,
                 marker=".",
             )
 
-    particles_p = [Particle(pd["proton"]) for _ in range(10)]
-    particles_e = [Particle(pd["electron"]) for _ in range(10)]
+    particles = [Particle(pd["electron"]) for _ in range(1)]
+    # particles.append(Particle(pd["proton"]))
 
     def animate_particle(t):
         log.info(next(n))
-        for particle in particles_p:
-            particle.calc_pos(t, particles_p, particles_e)
-        for particle in particles_e:
-            particle.calc_pos(t, particles_p, particles_e)
-
-        plt.cla()
-        for particle in particles_p:
-            particle.draw()
-        for particle in particles_e:
-            particle.draw()
+        for particle in particles:
+            particle.calculate_pos_v_a(particles)
+        if next(nn) % 1 == 0:
+            plt.cla()
+            for particle in particles:
+                particle.draw()
 
     def on_close_event(event):  # close function
         print(event)
